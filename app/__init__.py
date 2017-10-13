56cda9d55c544d7ca0557f31dd1e29ffc207d93d
Antje Kazimiers <kazimiersa@hhmi.org>
2017-10-13 12:30:25 -0400

    Add new project structure

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from mixer.backend.flask import mixer
+
+app = Flask(__name__)
+app.jinja_env.auto_reload = True
+app.config.from_object('config')
+
+@app.route('/')
+def landing_page():
+   return render_template('landing_page.html', tEvents=events)
+
+from app.models import Event
+
+with app.app_context():
+   events = mixer.cycle(4).blend(Event, name=(n for n in ('Back to School', 'Renovating', 'Race to end Poverty')))
---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+SECRET_KEY = 'my_secret_secret_key'
