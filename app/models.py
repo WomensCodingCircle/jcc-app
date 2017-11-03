c8b2d4c8d90edfc3c5596d18e601cc9e5a751bbc
Antje Kazimiers <kazimiersa@hhmi.org>
2017-11-03 12:58:15 -0400

    Donation table

--- app/models.py
+++ app/models.py
@@ -3,13 +3,31 @@ from app import app
 
 db = SQLAlchemy(app)
 
+class Event(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   date = db.Column(db.Date)
+   name = db.Column(db.String(150), unique = True, nullable=False)
+
+   def __repr__(self):
+      return self.name
+
+class Donation(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   personname=db.Column(db.String(150), nullable=False)
+   email = db.Column(db.String(120), unique=True, nullable=False)
+   employeeID=db.Column(db.Integer)
+   amount=db.Column(db.Float)
+
+   def __repr__(self):
+      return self.personname
+
 class User(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    username = db.Column(db.String(80), unique=True, nullable=False)
    email = db.Column(db.String(120), unique=True, nullable=False)
 
    def __repr__(self):
-      return '<User %r>' % self.username
+      return self.username
 
 class Contact(db.Model):
    id = db.Column(db.Integer, primary_key=True)
@@ -19,10 +37,5 @@ class Contact(db.Model):
    personal_phone = db.Column(db.String(20))
    personal_cellphone = db.Column(db.String(20))
 
-class Event(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   date = db.Column(db.Date)
-   name = db.Column(db.String(150), unique = True, nullable=False)
-
    def __repr__(self):
-      return '<User %r>' % self.name
+      return self.name
